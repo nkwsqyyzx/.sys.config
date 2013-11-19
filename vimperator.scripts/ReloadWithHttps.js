e83497e0c777b593b42e2b92d31dfb289bfda651
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2013-11-19 06:10:50 +0000

    add for seach faild & google failed

--- vimperator.scripts/ReloadWithHttps.js
+++ vimperator.scripts/ReloadWithHttps.js
@@ -3,11 +3,12 @@ commands.addUserCommand ("ReloadWithHttps", "reload url with https", function (a
     let target = d.getElementById('errorTitleText');
     if (target){
         let url = buffer.URL;
-        let b = /^http:\/\/www\.google\.com.+url\?.+url=/.test(url);
-        if (b) {
+        let google = /^http:\/\/www\.google\.com.+\/$/.test(url);
+        let result = /^http:\/\/www\.google\.com.+url\?.+url=/.test(url);
+        let search = /^http:\/\/www\.google\.com.+search\?.+&q=/.test(url);
+        if (google || result || search) {
             let httpsUrl = url.replace(/^http:/,"https:");
             liberator.open(httpsUrl);
-            return;
         }
     }
 });
