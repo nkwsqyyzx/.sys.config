e2c9008a98c6aec3fd47792d24ab516673cd601e
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2013-11-26 11:04:30 +0800

    add plugin & autocmd to delete titan ads

---
+++ vimperator.scripts/DeleteTitan007Ads.js
@@ -0,0 +1,40 @@
+commands.addUserCommand ("DeleteTitan007Ads", "delete titan007 ads", function (args) {
+    let d = tabs.getTab(args-1).linkedBrowser.contentDocument;
+    // 左右的广告
+    var a = d.getElementsByTagName('div');
+    for (i in a){
+        if (!a[i]){
+            continue;
+        }
+        var src = a[i].id;
+        var b = /^lqdel|r$/.test(src);
+        if (b) {
+            a[i].remove();
+        }
+    }
+
+    // 比分旁边的广告
+    a = d.getElementsByTagName('span');
+    for (i in a){
+        if (!a[i]){
+            continue;
+        }
+        var b = /^(left)|(right)_ad$/.test(a[i].id);
+        if (b) {
+            a[i].remove();
+        }
+    }
+
+    // 比分之间的广告
+    var a = d.getElementsByTagName('tr');
+    for (i in a){
+        if (!a[i]){
+            continue;
+        }
+        var b = /^tr_ad\d*$/.test(a[i].id);
+        if (b) {
+            a[i].remove();
+        }
+    }
+});
+/* vim:se sts=4 sw=4 et: */
