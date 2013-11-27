8ee4573262f147d202cb0529211693d973332358
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2013-11-27 16:32:06 +0800

    AdsKiller finished.

--- vimperator.scripts/AdsKiller.js
+++ vimperator.scripts/AdsKiller.js
@@ -1,7 +1,7 @@
-function element(d,tag,property,pattern){
+function element(d, tag, property, pattern) {
     var a = d.getElementsByTagName(tag);
     var rs = [];
-    for (var attr,i = 0,l = a.length;i<l;i++) {
+    for (var attr, i = 0, l = a.length; i < l; i++) {
         attr = a[i].getAttribute(property);
         if (attr === undefined)
             continue;
@@ -11,27 +11,44 @@ function element(d,tag,property,pattern){
     return rs;
 }
 
-commands.addUserCommand ("AdsKill", "kill adds", function (args) {
-    let d = tabs.getTab(args-1).linkedBrowser.contentDocument;
+commands.addUserCommand("AdsKill", "kill adds", function (args) {
+    let d = tabs.getTab(args - 1).linkedBrowser.contentDocument;
+    let body = d.body;
 
     // 内容中的广告
-    var divs = element(d,'div','class',/entry-?content/);
-    for (i in divs){
+    var c1 = 0;
+    var pattern = /(adsbygoogle|show_ads\.js|^http:\/\/pos\.baidu\.com)/;
+    var divs = element(d, 'div', 'class', /^entry(-?content)?$/);
+    for (i in divs) {
         var div = divs[i];
-        var suspect = element(div,'script','src',/adsbygoogle/);
+        var suspect = element(div, 'script', 'src', pattern);
         for (j in suspect) {
             var ad = suspect[j].parentNode;
             ad.remove();
+            c1++;
         }
     }
 
-    // 侧边栏的广告
-    var lis = element(d,'li','class',/^/);
-    for (i in lis){
-        var suspect = element(lis[i],'script','src',/adsbygoogle/);
-        for (j in suspect) {
-            suspect[j].parentNode.remove();
+    // 脚本广告
+    var c2 = 0;
+    var scriptAds = element(d, 'script', 'src', pattern);
+    for (i in scriptAds) {
+        var r = scriptAds[i].parentNode;
+        if (r == body) {
+            scriptAds[i].remove();
+        } else {
+            r.remove();
         }
+        c2++;
     }
+
+    // 两边悬浮广告
+    var c3 = 0;
+    var floatAds = element(d, 'div', 'id', /cproIframe\dholder/);
+    for (i in floatAds) {
+        floatAds[i].remove();
+        c3++;
+    }
+    if (c1 + c2 + c3 > 0) liberator.echo('移除内容中广告' + c1 + '个，移除边栏广告' + c2 + '个，悬浮广告' + c3 + '个');
 });
 /* vim:se sts=4 sw=4 et: */
