f499ffa46c79fd569b21d54dda088e2db684f6d3
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2013-12-03 08:40:22 +0000

    fix url pattern for google search results match

--- vimperator.scripts/ReloadWithHttps.js
+++ vimperator.scripts/ReloadWithHttps.js
@@ -3,15 +3,14 @@ commands.addUserCommand("ReloadWithHttps", "reload url with https", function (ar
     let target = d.getElementById('errorTitleText');
     if (target) {
         let url = buffer.URL;
-        let google = /^http:\/\/www\.google\.com.+\/$/.test(url);
-        let result = /^http:\/\/www\.google\.com.+url\?.+url=/.test(url);
-        let search = /^http:\/\/www\.google\.com.+search\?.{0,}q=/.test(url);
-        if (google || result || search) {
+        let result = /\burl=/.test(url);
+        let search = /\bsearch\?.{0,}q=/.test(url);
+        if (result || search) {
             let httpsUrl = url.replace(/^http:/, "https:");
             liberator.open(httpsUrl);
         }
     }
 });
 
-autocommands.add('DOMLoad', /google/, 'ReloadWithHttps <tab>');
+autocommands.add('DOMLoad', /^http:\/\/www\.google\.com/, 'ReloadWithHttps <tab>');
 /* vim:se sts=4 sw=4 et: */
