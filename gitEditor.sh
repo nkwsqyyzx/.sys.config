16fba62bc5d663bc36e3e15a8f056c4ca7abdd98
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2013-12-12 21:58:23 +0800

    fix *nix editing problem

--- gitEditor.sh
+++ gitEditor.sh
@@ -2,13 +2,23 @@
 
 # $SYS_OS is from system.detect.sh in .sys.config
 
-source ~/.sys.config/ConfigureVim.sh
 case $SYS_OS in
-    windows_mingw )
-        FILE_PATH="$1"
-        ;;
     windows_cygwin )
         FILE_PATH=`cygpath -a -w $1`
         ;;
+    * )
+        FILE_PATH="$1"
+        ;;
 esac
-gvim "$FILE_PATH"
+vim "$FILE_PATH"
+
+case $SYS_OS in
+    windows_* )
+        # first configure vim path for windows
+        source ~/.sys.config/ConfigureVim.sh
+        gvim "$1"
+        ;;
+    * )
+        vim "$1"
+        ;;
+esac
