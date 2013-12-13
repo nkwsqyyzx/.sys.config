3f6e43ef9d80d2c51d176bdd13655f479faa6449
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2013-12-13 10:08:07 +0000

    rewrite svn alias with function to avoid problem

--- settings.svn.sh
+++ settings.svn.sh
@@ -3,6 +3,21 @@ export SVN_EDITOR=vim
 alias up='svn up'
 alias st='svn st'
 alias sd='svn diff'
-alias svnremovedeletedfiles="svn st|grep '^!'|awk '/^!/ {print \$2}'|sed 's/\\\\/\\//g'|xargs svn remove --force"
-alias svnadduntrackedfiles="svn st|awk '/^?/ {print \$2}'|sed 's/\\\\/\\//g'|xargs svn add --force"
-alias svnremoveaddedfiles="svn st|awk '/^A/ {print \$2}'|sed 's/\\\\/\\//g'|xargs svn remove --keep-local --force"
+svnremovedeletedfiles()
+{
+    svn st|awk '/^!/ {print $2}'|sed 's#\\#\/#g'|while read -r file;
+do
+    svn remove --force "$file"
+done
+}
+svnadduntrackedfiles()
+{
+    svn st|awk '/^?/ {print $2}'|sed 's#\\#\/#g'|xargs svn add --force
+}
+svnremoveaddedfiles()
+{
+    svn st|awk '/^A/ {print $2}'|tac|sed 's#\\#\/#g'|while read -r file;
+do
+    svn remove --keep-local --force "$file"
+done
+}
