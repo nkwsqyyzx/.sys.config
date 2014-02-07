5bd720c711bf90338310237a3fae6433901dc83b
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2014-02-07 16:43:05 +0800

    add L to less output

--- settings.svn.sh
+++ settings.svn.sh
@@ -3,6 +3,7 @@ export SVN_EDITOR=vim
 alias up='svn up'
 alias st='svn st'
 alias sd='svn diff'
+alias sdv='svn diff|gvim -R -'
 svnremovedeletedfiles()
 {
     svn st|awk '/^!/ {print $2}'|sed 's#\\#\/#g'|while read -r file;
