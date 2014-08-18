4b018ab8e6d8030d03d855f902c4c428f48ab1b5
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2014-08-18 13:46:13 +0800

    Add function to show adb log & export lang settings for mac.

--- tool.function.sh
+++ tool.function.sh
@@ -7,3 +7,8 @@ function deletedb
 {
     for i in $(adb shell ls /data/data/$ANDROIDPRO/databases|dos2unix|grep '^\d');do adb shell rm "/data/data/$ANDROIDPRO/databases/$i";done
 }
+
+function adblogcv
+{
+    adb logcat -C -v time $ANDROIDPRO|LANG=C LC_CTYPE=C sed -e '/DexOpt/d' -e '/dalvikvm/d' -e '/I\/System.out/d' -e '/PackageManager/d' -e '/W\/System.err/d' -e '/[IW]\/ActivityManager/d' -e '/bmpCache/d'
+}
