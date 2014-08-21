576e797fa5457fae275e22559aac9170620cc695
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2014-08-21 10:33:03 +0800

    Upgrade logcat.

--- tool.function.sh
+++ tool.function.sh
@@ -1,14 +1,14 @@
 function pulldb
 {
-    for i in $(adb shell ls /data/data/$ANDROIDPRO/databases|dos2unix|grep '^\d');do adb pull "/data/data/$ANDROIDPRO/databases/$i";done
+    for i in $(adb shell ls /data/data/$ANDROIDPRO/databases|dos2unix|grep '^message_\d');do adb pull "/data/data/$ANDROIDPRO/databases/$i";done
 }
 
 function deletedb
 {
-    for i in $(adb shell ls /data/data/$ANDROIDPRO/databases|dos2unix|grep '^\d');do adb shell rm "/data/data/$ANDROIDPRO/databases/$i";done
+    for i in $(adb shell ls /data/data/$ANDROIDPRO/databases|dos2unix|grep '^message_\d');do adb shell rm "/data/data/$ANDROIDPRO/databases/$i";done
 }
 
 function adblogcv
 {
-    adb logcat -C -v time $ANDROIDPRO|LANG=C LC_CTYPE=C sed -e '/DexOpt/d' -e '/dalvikvm/d' -e '/I\/System.out/d' -e '/PackageManager/d' -e '/W\/System.err/d' -e '/[IW]\/ActivityManager/d' -e '/bmpCache/d'
+    adb logcat -C -v time $ANDROIDPRO|LANG=C LC_CTYPE=C sed -n -e '/LSH:/p' -e '/AndroidRuntime/p'
 }
