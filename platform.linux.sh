9b9709d88a7c04b498243ee62cbbbf5ec5c612ad
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2015-04-22 17:25:34 +0800

    Add ip alias to view local ip.

--- platform.linux.sh
+++ platform.linux.sh
@@ -1,3 +1,4 @@
 alias o='nautilus'
 alias ou='xdg-open'
 alias ll='ls -al --color=auto'
+alias ip="ifconfig|sed -n -e '/127.0.0.1/d' -e '/inet /p'|awk -F: '{print \$2}'|awk '{print \$1}'"
--- platform.mac.sh
+++ platform.mac.sh
@@ -1,5 +1,6 @@
 alias o='open'
 alias ou='open'
 alias ll='ls -alG'
+alias ip="ifconfig | sed -n -e '/127.0.0.1/d' -e '/inet /p'|awk '{print \$2}'"
 
 export DIFFMERGE_EXE=/Applications/DiffMerge.app/Contents/MacOS/DiffMerge
