e192f1c6a945f35acb33c6d5fa0241adab22e01a
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2015-06-30 01:46:38 +0100

    Add kill firefox for windows.

--- platform.windows_common.sh
+++ platform.windows_common.sh
@@ -1,4 +1,4 @@
 export LESSCHARSET=utf-8
 
 # windows common alias for
-alias kfirefox='taskkill -f -pid $(tasklist|grep ^firefox|awk "{print \$2}")'
+alias kfirefox='taskkill /f /pid $(tasklist|grep ^firefox|awk "{print \$2}")'
