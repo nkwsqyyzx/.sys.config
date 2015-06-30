580531eb6e4f34a2af80dedcdbf32c510263c0d3
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2015-06-30 01:46:10 +0100

    Add cache support for network.

--- bin/sjxs.py
+++ bin/sjxs.py
@@ -4,10 +4,13 @@
 import urllib2
 from bs4 import BeautifulSoup
 import sys
+from cache import HtmlCache
+
+cache = HtmlCache('/tmp/')
 
 def get(code):
     url = 'http://f10.eastmoney.com/f10_v2/ShareholderResearch.aspx?code=sh' + code
-    content = ''.join(urllib2.urlopen(url).readlines())
+    content = cache.getContent(url, timeout = 60*60*24*12)[0]
     soup = BeautifulSoup(content)
     rate = soup.find('div', attrs={'id':'TTCS_Table_Div', 'class':'content first'}).find('table').findAll('tr')[-1].findAll('td')[-3].getText()
     rate = float(rate.split('%')[0]) / 100.0
