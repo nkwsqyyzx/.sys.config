5002313ed7f5a7dba105e524b68f4431c17e3008
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2015-08-19 13:38:27 +0800

    Socks proxy for osx.

--- platform.mac.sh
+++ platform.mac.sh
@@ -4,3 +4,30 @@ alias ll='ls -alG'
 alias ip="ifconfig | sed -n -e '/127.0.0.1/d' -e '/inet /p'|awk '{print \$2}'"
 
 export DIFFMERGE_EXE=/Applications/DiffMerge.app/Contents/MacOS/DiffMerge
+
+function proxy()
+{
+	case "$1" in
+	on)
+		sudo networksetup -setsocksfirewallproxystate Wi-Fi on
+		;;
+	off)
+		sudo networksetup -setsocksfirewallproxystate Wi-Fi off
+		;;
+	set)
+		local domain="$2"
+		local port="$3"
+		if [ -z "$domain" ] || [ -z "$port" ]; then
+			echo "Usage: proxy set domain port"
+		else
+			sudo networksetup -setsocksfirewallproxy Wi-Fi "$domain" "$port"
+		fi
+		;;
+	status|st)
+		networksetup -getsocksfirewallproxy Wi-Fi
+		;;
+	*)
+		echo "Usage: proxy {on|off|set|status}"
+		;;
+	esac
+}
