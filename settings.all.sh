32552f87c768d6b7136bca73488e8fe3baf9ff51
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2016-02-15 11:45:22 +0800

    Add tool for find & xargs grep.

--- settings.all.sh
+++ settings.all.sh
@@ -3,6 +3,7 @@ ROOT="$BASEDIR"/settings/
 source "$ROOT"/android.sh
 source "$ROOT"/find.sh
 source "$ROOT"/git.sh
+source "$ROOT"/grep.sh
 source "$ROOT"/locales.sh
 source "$ROOT"/misc.sh
 source "$ROOT"/svn.sh
