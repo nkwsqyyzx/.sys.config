cfdc3a0a76581cd6a693195e88f596dfdd1d88c5
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2016-09-19 15:08:25 +0800

    Fix xgrep with white space filename

--- settings/grep.sh
+++ settings/grep.sh
@@ -1,9 +1,9 @@
 # find & grep by file extension
 function xgrep() {
     if [[ "$#" -eq 3 ]]; then
-        find "$1" -type f -name "*.$2"|xargs grep -n "$3"
+        find "$1" -type f -name "*.$2" -print0 | xargs -0 grep -n "$3"
     elif [[ "$#" -eq 2 ]]; then
-        find . -type f -name "*.$1"|xargs grep -n "$2"
+        find . -type f -name "*.$1" -print0 | xargs -0 grep -n "$2"
     else
         echo "Useage:xgrep [DIRECTORY] <file-extension> <pattern>"
     fi
