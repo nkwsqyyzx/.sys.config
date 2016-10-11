a1243eced520688314a4b6d7c331b6f1bb1f16f8
zhangxingyu <zhangxingyu@baidu.com>
2016-10-11 14:36:42 +0800

    quick commmand to switch between charles and google

--- platform.mac.sh
+++ platform.mac.sh
@@ -5,6 +5,27 @@ alias ip="ifconfig | sed -n -e '/127.0.0.1/d' -e '/inet /p'|awk '{print \$2}'"
 
 export DIFFMERGE_EXE=/Applications/DiffMerge.app/Contents/MacOS/DiffMerge
 
+# Follow this page to avoid enter password
+# http://apple.stackexchange.com/questions/236806/prevent-networksetup-from-asking-for-password
+function proxy-charles()
+{
+    sudo networksetup -setwebproxystate Wi-Fi on;
+    sudo networksetup -setsecurewebproxystate  Wi-Fi on;
+    sudo networksetup -setwebproxy Wi-Fi 127.0.0.1 8888;
+    sudo networksetup -setsecurewebproxy Wi-Fi 127.0.0.1 8888;
+    sudo networksetup -setautoproxystate Wi-Fi off;
+    sudo networksetup -setsocksfirewallproxystate Wi-Fi off;
+}
+
+function proxy-google()
+{
+    sudo networksetup -setwebproxystate Wi-Fi off;
+    sudo networksetup -setsecurewebproxystate  Wi-Fi off;
+    sudo networksetup -setautoproxystate Wi-Fi on;
+    sudo networksetup -setautoproxyurl Wi-Fi http://pac.internal.baidu.com/bdnew.pac;
+    sudo networksetup -setsocksfirewallproxystate Wi-Fi off;
+}
+
 function proxy()
 {
 	case "$1" in
