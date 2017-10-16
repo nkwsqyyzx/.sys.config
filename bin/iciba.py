5a611ef682bb653c75ba7b5d2d983dd767df389e
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2017-10-16 14:01:06 +0800

    Format python file header & space

--- bin/iciba.py
+++ bin/iciba.py
@@ -1,17 +1,18 @@
-#!env python
+#!/usr/bin/env python
 
 import bs4
 import urllib2
 import sys
 
-BASE_URL='http://open.iciba.com/huaci/dict.php?word='
+BASE_URL = 'http://open.iciba.com/huaci/dict.php?word='
+
 
 def dig(word):
     quote = urllib2.quote(word)
     html = urllib2.urlopen(BASE_URL + quote).readlines()
     soup = bs4.BeautifulSoup(''.join(html), "html.parser")
     found = False
-    for div in soup.findAll('div', {'class':'\\"icIBahyI-group_pos\\"'}):
+    for div in soup.findAll('div', {'class': '\\"icIBahyI-group_pos\\"'}):
         for p in div.findAll('p'):
             print(p.get_text().encode('utf8'))
             found = True
@@ -19,5 +20,6 @@ def dig(word):
     if not found:
         print("I can't dig \033[31m{0}\033[0m in iciba.".format(word))
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     dig(' '.join(sys.argv[1:]))
