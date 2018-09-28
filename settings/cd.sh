5cabb236418fe2b9b6e3987c519a885a316689c8
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2018-09-28 15:45:26 +0800

    Use top 1/3 entries in autojump to support fzf enter directory

--- settings/cd.sh
+++ settings/cd.sh
@@ -2,7 +2,10 @@ export AUTOJUMP_WEIGHT_FILE="$(type j 1>/dev/null 2>/dev/null && j -s | tail -1 
 
 function _print_selected_dir() {
     if [[ -r "${AUTOJUMP_WEIGHT_FILE}" ]]; then
-        cat "${AUTOJUMP_WEIGHT_FILE}" | sort -nr | column -F'\t' 2 | fzf +s
+        if [[ -z "${AUTOJUMP_WEIGHT_FILE_LINES}" ]]; then
+            export AUTOJUMP_WEIGHT_FILE_LINES=$(wc -l "${AUTOJUMP_WEIGHT_FILE}"|column 1)
+        fi
+        sort -nr "${AUTOJUMP_WEIGHT_FILE}" | head -n $((${AUTOJUMP_WEIGHT_FILE_LINES}/3)) | column -F'\t' 2 | fzf +s
     fi
 }
 
