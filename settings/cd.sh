9e26a8a0790f5b10de441ef523cc31ab6b41e8fd
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2019-02-26 15:56:36 +0800

    Update cd funtcion to avoid using custom function
    Conflicts:
    	settings/cd.sh

--- settings/cd.sh
+++ settings/cd.sh
@@ -1,16 +1,16 @@
-export AUTOJUMP_WEIGHT_FILE="$(type j 1>/dev/null 2>/dev/null && j -s | tail -1 | column 2)"
+export AUTOJUMP_WEIGHT_FILE="$(type autojump 1>/dev/null 2>/dev/null && autojump -s | tail -1 | awk '{print $2}')"
 
 function _print_selected_dir() {
     if [[ -r "${AUTOJUMP_WEIGHT_FILE}" ]]; then
         if [[ -z "${AUTOJUMP_WEIGHT_FILE_LINES}" ]]; then
-            local lines=$(wc -l "${AUTOJUMP_WEIGHT_FILE}"|column 1)
+            local lines=$(wc -l "${AUTOJUMP_WEIGHT_FILE}"|awk '{print $1}')
             lines="$((${lines}/2))"
             if [[ ${lines} -lt 100 ]]; then
                 lines=100
             fi
             export AUTOJUMP_WEIGHT_FILE_LINES=${lines}
         fi
-        sort -nr "${AUTOJUMP_WEIGHT_FILE}" | head -n $((${AUTOJUMP_WEIGHT_FILE_LINES})) | column -F'\t' 2 | fzf +s
+        sort -nr "${AUTOJUMP_WEIGHT_FILE}" | head -n $((${AUTOJUMP_WEIGHT_FILE_LINES})) | awk '{print $2}' | fzf +s
     fi
 }
 
