b678433b420724ccefc285258b517a7c491be4d4
nkwsqyyzx <nkwsqyyzx@gmail.com>
2026-07-22 10:24:22 +0800

    feat(platform.linux): 加 Linuxbrew shellenv 到 PATH
    eval (brew shellenv) 设置 PATH/MANPATH/INFOPATH 等运行时变量.
    仅在 /home/linuxbrew/.linuxbrew/bin/brew 存在时生效 (其他 Linux 无影响).

--- platform.linux.sh
+++ platform.linux.sh
@@ -1,5 +1,10 @@
-alias o='nautilus'
-alias ou='xdg-open'
-alias ll='ls -al --color=auto'
+# Linuxbrew (canonical env setup: PATH + INFOPATH etc.)
+if [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
+alias o="nautilus"
+alias ou="xdg-open"
+alias ll="ls -al --color=auto"
 alias ip="ifconfig|sed -n -e '/127.0.0.1/d' -e '/inet /p'|awk -F: '{print \$2}'|awk '{print \$1}'"
-alias ifconfig='/sbin/ifconfig'
+alias ifconfig="/sbin/ifconfig"
